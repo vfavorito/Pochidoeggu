b7635236fda4bcd9081502f765f57a18dccdf05c
vfavorito <vince.favorito@gmail.com>
2021-01-15 12:59:15 -0600

    Created Account model with a username and password

--- models/account.js
+++ models/account.js
@@ -1,13 +1,23 @@
 // bcrypt is used to scramble the passwords we save in database
 const bcrypt = require("bcryptjs");
 module.exports = function (sequelize, DataTypes) {
-    // need to set up Account model/table here
-    const Account = sequelize.define("Account", {
-
-
-
-
-    });
+    let Account = sequelize.define("Account", {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                len: [5, 20]
+            }
+        },
+        password: {
+            type: DataTypes.String,
+            allowNull: false
+        }
+    },
+        {
+            freezeTableName: true
+        });
     // adding a prototype function to the model to be used in the localStrategy for passport in passport.js file
     Account.prototype.validPassword = function (password) {
         return bcrypt.compareSync(password, this.password);
@@ -16,6 +26,5 @@ module.exports = function (sequelize, DataTypes) {
     Account.addHook("beforeCreate", (account) => {
         account.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null)
     });
-    
     return Account
 }
